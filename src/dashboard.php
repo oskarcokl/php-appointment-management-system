eb2601362bb6bd66e2bea5ae3bf493e33da4f0c5
coskar <oskar.cokl@iprom.si>
2024-07-19 22:37:47 +0200

    feat: Add calendar to dashbaord.

--- src/dashboard.php
+++ src/dashboard.php
@@ -17,15 +17,29 @@ if (isset($_POST) && array_key_exists('logOut', $_POST)) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.14/index.global.min.js'></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth'
+            });
+            calendar.render();
+        });
+    </script>
     <title>Dashboard</title>
 </head>
+
 <body>
     <h1>Your appointments dashboard</h1>
+    <div id='calendar'></div>
     <form action="dashboard.php" method="post">
         <button type="submit" name="logOut">log out</button>
     </form>
 </body>
+
 </html>
